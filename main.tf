f90ba8d67ddbc7562aa005de64f4eb62999c20c8
nissiprasanthi121 <nissiprasanthi121@gmail.com>
2025-03-28 11:06:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-08fe5144e4659a3b3"
+ami = "ami-01bd9d8f06d29d6a0"
 instance_type = "t2.medium"
 key_name = "nissi05"
 vpc_security_group_ids = ["sg-078d5c46a3a27aa33"]
