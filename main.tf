93dc6ad2fdbea8bb72a4ab71c78526d6ba72bdb0
nissiprasanthi121 <nissiprasanthi121@gmail.com>
2025-03-28 11:49:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-01bd9d8f06d29d6a0"
 instance_type = "t2.medium"
 key_name = "nissi05"
-vpc_security_group_ids = ["sg-078d5c46a3a27aa33"]
+vpc_security_group_ids = ["sg-02abeb7c06968994a"]
 tags = {
 Name = var.instance_names[count.index]
 }
